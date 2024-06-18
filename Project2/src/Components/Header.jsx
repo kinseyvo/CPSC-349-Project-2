29156a1bd590b7edd42b2a8f5234f86b4a4e9012
kinseyvo <kinseyvo@csu.fullerton.edu>
2024-06-18 13:41:53 -0700

    minor navbar fixes

--- Project2/src/Components/Header.jsx
+++ Project2/src/Components/Header.jsx
@@ -4,14 +4,14 @@ import style from "../styles/header.module.css";
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth <= 768);
         };
 
-        handleResize();
+        //handleResize();
         window.addEventListener("resize", handleResize);
 
         return () => window.removeEventListener("resize", handleResize);
@@ -26,7 +26,10 @@ export default function Header() {
         <div className={style.navbar}>
             <Link to="/" className={style.navbarName}>KV</Link>
             <div className={style.menuContainer}>
-                <button className={style.hamburgerMenu} onClick={toggleMenu}>
+                <button 
+                    className={`${style.hamburgerMenu} ${isMenuOpen ? style.open : ""}`}
+                    onClick={toggleMenu}
+                >
                     <span className={style.hamburgerIcon}></span>
                 </button>
                 
@@ -47,16 +50,6 @@ export default function Header() {
                         <Link to="/contact" className={style.navbarLink}>Contact Me</Link>
                     </div>
                 )}
-
-                {/* NavBar Dropdown Code
-                {(isMenuOpen || !isMobile) && ( // checks if menu is open or if it's not mobile
-                    <div className={isMenuOpen ? `${style.navbarLinks} ${style.showMenu}` : style.navbarLinks}>
-                        <Link to="/home" className={style.navbarLink}>Home</Link>
-                        <Link to="/projects" className={style.navbarLink}>Projects</Link>
-                        <Link to="/about" className={style.navbarLink}>About</Link>
-                        <Link to="/contact" className={style.navbarLink}>Contact Me</Link>
-                    </div>
-                )} */}
             </div>
         </div>
     );
